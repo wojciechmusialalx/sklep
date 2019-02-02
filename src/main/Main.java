c73a69ea306144cfd00dd3c495b8201c6f6a322f
Wojciech Musiał <wojciech_musial@poczta.fm>
2019-02-02 10:44:38 +0100

    zaimplementowane menu sklepu

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import ui.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menuAplikacji = new Menu();
+        menuAplikacji.wyswietl();
+    }
+}
---
+++ src/ui/Menu.java
@@ -0,0 +1,39 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void wyswietl() {
+        Scanner wejscie = new Scanner(System.in);
+
+        System.out.println("1 - dodaj alkohol");
+        System.out.println("2 - usun alkohol");
+        System.out.println("3 - wyswietl");
+        System.out.println("q - wyjscie");
+
+        String wybor;
+
+        do {
+            System.out.print("Podaj wybor: ");
+            wybor = wejscie.nextLine();
+
+            switch (wybor) {
+                case "1":
+                    System.out.println("dodawanie produktu");
+                    break;
+                case "2":
+                    System.out.println("usun");
+                    break;
+                case "3":
+                    System.out.println("wyswietl");
+                    break;
+                case "q":
+                    System.out.println("Koniec programu");
+                    break;
+                default:
+                    System.out.println("Bledna opcja, sprobuj jeszcze raz");
+            }
+        } while (!wybor.equals("q"));
+    }
+}
